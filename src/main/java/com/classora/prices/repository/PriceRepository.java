3a1fbf241a0259f3218088833a17b8c4c102a419
Miguel Angel Lorden Feas <miguelangellordendev@gmail.com>
2026-08-29 23:37:49 +0200

    T5 feat: price repository

--- src/main/java/com/classora/prices/repository/PriceRepository.java
+++ src/main/java/com/classora/prices/repository/PriceRepository.java
@@ -1,4 +1,22 @@
 package com.classora.prices.repository;
 
-public class PriceRepository {
+import com.classora.prices.model.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PriceRepository extends JpaRepository<Price, Long> {
+
+    @Query("SELECT p FROM Price p " +
+            "WHERE p.brandId = :brandId " +
+            "AND p.productId = :productId " +
+            "AND :applicationDate BETWEEN p.startDate AND p.endDate " +
+            "ORDER BY p.priority DESC")
+    List<Price> findApplicablePrices(@Param("brandId") Long brandId,
+                                     @Param("productId") Long productId,
+                                     @Param("applicationDate") LocalDateTime applicationDate);
+
 }
