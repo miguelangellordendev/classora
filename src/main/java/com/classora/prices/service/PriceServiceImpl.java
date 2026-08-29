da11d1b71f674b27ca719d50e4c3d7ea026d762a
Miguel Angel Lorden Feas <miguelangellordendev@gmail.com>
2026-08-30 00:02:17 +0200

    T6 feat: service logic and exception

--- src/main/java/com/classora/prices/service/PriceServiceImpl.java
+++ src/main/java/com/classora/prices/service/PriceServiceImpl.java
@@ -1,14 +1,13 @@
 package com.classora.prices.service;
 
 import com.classora.prices.dto.PriceResponse;
+import com.classora.prices.exceptions.PriceNotFoundException;
 import com.classora.prices.mapper.PriceMapper;
-import com.classora.prices.model.Price;
 import com.classora.prices.repository.PriceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,11 +19,10 @@ public class PriceServiceImpl implements PriceService {
 
     @Override
     public PriceResponse getApplicablePrice(Long brandId, Long productId, LocalDateTime applicationDate) {
-        Optional<Price> applicablePrice = priceRepository
+        return priceMapper.toResponse(priceRepository
                 .findApplicablePrices(brandId, productId, applicationDate)
                 .stream()
-                .findFirst();
-
-        return null;
+                .findFirst()
+                .orElseThrow(() -> new PriceNotFoundException(productId, brandId, applicationDate.toString())));
     }
 }
