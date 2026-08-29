fccec7907431bb68d126a92f9820fdac9d0290c5
Miguel Angel Lorden <miguelangellordendev@gmail.com>
2026-08-29 23:06:15 +0200

    T3 feat: price model (#3)

--- src/main/java/com/classora/prices/model/Price.java
+++ src/main/java/com/classora/prices/model/Price.java
@@ -1,4 +1,127 @@
 package com.classora.prices.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "PRICES")
 public class Price {
+
+    public Price(Long id, Long brandId, LocalDateTime startDate, LocalDateTime endDate, Long priceList, Long productId, Integer priority, BigDecimal price, String curr) {
+        this.id = id;
+        this.brandId = brandId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.priceList = priceList;
+        this.productId = productId;
+        this.priority = priority;
+        this.price = price;
+        this.curr = curr;
+    }
+
+    public Price() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "BRAND_ID", nullable = false)
+    private Long brandId;
+
+    @Column(name = "START_DATE", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "END_DATE", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "PRICE_LIST", nullable = false)
+    private Long priceList;
+
+    @Column(name = "PRODUCT_ID", nullable = false)
+    private Long productId;
+
+    @Column(name = "PRIORITY", nullable = false)
+    private Integer priority;
+
+    @Column(name = "PRICE", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "CURR", nullable = false, length = 3)
+    private String curr;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public Long getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Long brandId) {
+        this.brandId = brandId;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public Long getPriceList() {
+        return priceList;
+    }
+
+    public void setPriceList(Long priceList) {
+        this.priceList = priceList;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCurr() {
+        return curr;
+    }
+
+    public void setCurr(String curr) {
+        this.curr = curr;
+    }
 }
