18932bcfa4b30ddb656614c0b7821e09cf405a0a
Miguel Angel Lorden Feas <miguelangellordendev@gmail.com>
2026-08-29 23:52:41 +0200

    T6 feat: service first version

--- src/main/java/com/classora/prices/service/PriceServiceImpl.java
+++ src/main/java/com/classora/prices/service/PriceServiceImpl.java
@@ -1,4 +1,27 @@
 package com.classora.prices.service;
 
-public class PriceServiceImpl {
+import com.classora.prices.dto.PriceResponse;
+import com.classora.prices.model.Price;
+import com.classora.prices.repository.PriceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PriceServiceImpl implements PriceService {
+
+    private final PriceRepository priceRepository;
+
+    @Override
+    public PriceResponse getApplicablePrice(Long brandId, Long productId, LocalDateTime applicationDate) {
+        Optional<Price> applicablePrice = priceRepository
+                .findApplicablePrices(brandId, productId, applicationDate)
+                .stream()
+                .findFirst();
+
+        return null;
+    }
 }
